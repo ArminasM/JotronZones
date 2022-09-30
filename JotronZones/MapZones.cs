16392c9291c227d9ee992dbc66be9f3881614145
Armis <arminas.mil@gmail.com>
2022-09-30 10:30:29 +0300

    Add project files.

---
+++ JotronZones/MapZones.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Text;
+using System.IO;
+using System.Security.Claims;
+
+namespace JotronZones
+{
+    class MapZones
+    {
+
+
+
+        public static int Main(string[] args)
+        {
+            if (Path.GetExtension(args[0]) != ".map") throw new InvalidDataException("Please supply correct data format");
+
+            string[] lines = File.ReadAllLines(args[0]);
+
+            Console.WriteLine(lines.Length);
+
+
+
+
+            return 0;
+        }
+
+
+    }
+
+}
+
+
+
+
+
