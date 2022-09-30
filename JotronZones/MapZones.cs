4d1895bb3a689f7c3047679b651cdc225b37ba9d
Armis <arminas.mil@gmail.com>
2022-09-30 10:38:00 +0300

    Implemented reading files and trimming white spaces.

--- JotronZones/MapZones.cs
+++ JotronZones/MapZones.cs
@@ -3,24 +3,20 @@ using System;
 using System.Text;
 using System.IO;
 using System.Security.Claims;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace JotronZones
 {
     class MapZones
     {
-
-
-
         public static int Main(string[] args)
         {
             if (Path.GetExtension(args[0]) != ".map") throw new InvalidDataException("Please supply correct data format");
 
             string[] lines = File.ReadAllLines(args[0]);
 
-            Console.WriteLine(lines.Length);
-
-
-
+            lines = lines.Select(line => Regex.Replace(line, @"\s+", " ")).ToArray();
 
             return 0;
         }
